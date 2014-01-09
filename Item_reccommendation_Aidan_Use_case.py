cc565926cae269202ed4eecfc555bbc2750bc0a9
James Murphy <jm36@student.cs.ucc.ie>
2014-01-09 15:59:25 +0000

    Updated with new data structures

--- Item_reccommendation_Aidan_Use_case.py
+++ Item_reccommendation_Aidan_Use_case.py
@@ -9,6 +9,8 @@ from random import shuffle
 import MySQLdb
 import math
 import MySQLdb.cursors
+from operator import itemgetter
+from heapq import heappush, heappop, heappushpop
 
 numItems = 120000 
 
@@ -20,7 +22,7 @@ db = MySQLdb.connect(host="localhost", # your host, usually localhost
                      cursorclass=MySQLdb.cursors.DictCursor) 
 
                      
-matrix = lil_matrix((120000, 42000))
+itemMatrix = lil_matrix((120000, 42000))
 
 cur = db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
 curSong = db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
@@ -28,40 +30,35 @@ currentSong = db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
 cur.execute("SELECT songid, userid from actions where type = 'PLAY' order by userid ASC;")
 rows = cur.fetchall()
 for row in rows:
-    matrix[row['songid'],row['userid']] = 1 
+    itemMatrix[row['songid'],row['userid']] = 1 
 
 print "Matrix set up." 
 
-topItems = []
-
-Similarity = {}
-currentUserMatrix = matrix.getrow(songId)	  
+Similarity = []
+currentItemMatrix = itemMatrix.getrowview(songId)      
 for j in range(1, numItems):
-    tempUserMatrix = matrix.getrow(j)
-    if songId == j:
-	    Similarity[j] = 0
-    else:
-	if len(currentUserMatrix.rows[0]) >= 20:
-	    Similarity[j] = cosine_similarity(currentUserMatrix, tempUserMatrix)
-	else:
-	    Similarity[j] = (cosine_similarity(currentUserMatrix, tempUserMatrix)/20)
+    tempItemMatrix = itemMatrix.getrowview(j)
+    if j != songId:
+        if j <= 10:
+            temp = cosine_similarity(currentItemMatrix, tempItemMatrix)
+            heappush(Similarity, ( temp[0][0], j ))
+        else:    
+            temp = cosine_similarity(currentItemMatrix, tempItemMatrix)
+            if temp[0][0] > Similarity[0][0]:
+                heappushpop(Similarity, ( temp[0][0], j))
 
 
-topItems = heapq.nlargest(5, Similarity, key=Similarity.get)
-#print "Top users", topUsers
-
-print "Top items", topItems
 
 
-#print finalList    
-#print finalSong
 currentSong.execute("Select artist, title from songs where songid = %s;", int(songId))
 result = currentSong.fetchall()
 for res in result:
     print "These reccommendations are based on song: {0} by {1}".format(res['title'] , res['artist'])
 #print song
-for item in topItems:
-    curSong.execute("Select artist, title from songs where songid = %s;", int(item))
+
+for n in range(10):
+    song = heappop(Similarity)
+    curSong.execute("Select artist, title from songs where songid = %s;", song[1])
     resultSong = curSong.fetchall()
     for resSong in resultSong:
 	print "The system reccommends song : {0} by {1} ".format(resSong['title'] , resSong['artist'])
