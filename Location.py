c0071775ce7919715767debf0acfbb27e00506e8
James Murphy <jm36@student.cs.ucc.ie>
2014-02-18 19:15:53 +0000

    Updated

--- Location.py
+++ Location.py
@@ -22,7 +22,7 @@ db = MySQLdb.connect(host="localhost", # your host, usually localhost
 
 
 cur = db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
-cur.execute("SELECT userid, lon, lat from actions where userid = 6;");
+cur.execute("SELECT userid, lon, lat from actions where userid = 8;");
 
 cursorResults = cur.fetchall()
 resultList = []
@@ -30,27 +30,21 @@ for rowOrig in cursorResults:
     if rowOrig['lon'] != 0 :
         temp = rowOrig['lon'], rowOrig['lat']
         resultList.append(temp)
-#X = np.array(resultList)      
+    
 
 X = StandardScaler().fit_transform(resultList)
 print X
     
-# Compute DBSCAN
-db = DBSCAN(eps=0.3, min_samples=5 ).fit(X)
+
+db = DBSCAN(eps=0.3, min_samples=2 ).fit(X)
 core_samples = db.core_sample_indices_
 labels = db.labels_
 
-# Number of clusters in labels, ignoring noise if present.
+
 n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
 
 print('Estimated number of clusters: %d' % n_clusters_)
-#print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
-#print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
-#print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
-#print("Adjusted Rand Index: %0.3f"
- #     % metrics.adjusted_rand_score(labels_true, labels))
-#print("Adjusted Mutual Information: %0.3f"
-#      % metrics.adjusted_mutual_info_score(labels_true, labels))
+
 print("Silhouette Coefficient: %0.3f"
       % metrics.silhouette_score(X, labels))
 
@@ -58,12 +52,12 @@ print("Silhouette Coefficient: %0.3f"
 # Plot result
 import pylab as pl
 
-# Black removed and is used for noise instead.
+
 unique_labels = set(labels)
 colors = pl.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
 for k, col in zip(unique_labels, colors):
     if k == -1:
-        # Black used for noise.
+
         col = 'k'
         markersize = 6
     class_members = [index[0] for index in np.argwhere(labels == k)]
